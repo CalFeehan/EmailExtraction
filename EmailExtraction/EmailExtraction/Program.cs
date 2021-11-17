15cfcd355046d420c02531a6e0c3c8fe83042d7c
Callum Feehan <Callum.Feehan@IRIS.CO.UK>
2021-11-17 14:27:31 +0000

    inital functionality

--- EmailExtraction/EmailExtraction/Program.cs
+++ EmailExtraction/EmailExtraction/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace EmailExtraction
 {
@@ -6,7 +10,36 @@ namespace EmailExtraction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"C:\Users\Callum.Feehan\OneDrive\Documents\Apprenticeship\C Sharp Bootcamp\EmailExtraction\EmailExtraction\EmailExtraction\text.txt";
+            string text = File.ReadAllText(path);
+
+            Regex rx = new Regex(@"[\.\'\%\+\-a-zA-Z0-9_]+@[\.\'\%\+\-a-zA-Z0-9_]+\.[a-zA-Z\.]+[ \n\t]+");
+
+            // int count = rx.Matches(text).Count();
+            MatchCollection matches = rx.Matches(text);
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (Match match in matches)
+            {
+                string emailEnd = match.Value;
+                emailEnd = emailEnd.Substring(emailEnd.IndexOf("@"));
+
+                if (dict.ContainsKey(emailEnd)){
+                    dict[emailEnd] += 1;
+                } else
+                {
+                    dict.Add(emailEnd, 1);
+                }
+            }
+
+            foreach (KeyValuePair<string, int> pair in dict.OrderByDescending(key => key.Value))
+            {
+                Console.WriteLine($"Key {pair.Key} Value {pair.Value}");
+            }
+
+            for (int i = 0; i < 10; i++) {
+                Console.WriteLine(dict.OrderByDescending(key => key.Value).ElementAt(i));
+            }
         }
     }
 }
